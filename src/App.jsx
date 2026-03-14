579ce645abe34b0b1785494233f22222e067db6f
jookem <tegamikureru@hotmail.com>
2026-03-14 09:16:44 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3812,7 +3812,11 @@ const paginated = filtered
                   fontFamily: "'Space Mono',monospace",
   }}
 >
-   <img src="/images/plan.svg" alt="plan" style={{ height: 16, width: 16 }} />
+  <img src="/images/plan.svg" alt="plan" style={{ 
+  height: 16, 
+  width: 16,
+  filter: isDark ? "brightness(0) invert(1)" : "invert(27%) sepia(89%) saturate(400%) hue-rotate(116deg) brightness(96%) contrast(101%)"
+}} />
   PLAN
 </button>
             </div>
@@ -4034,7 +4038,7 @@ const paginated = filtered
     gap: 6,
   }}
 >
-  <img src="/images/plan.svg" alt="plan" style={{ height: 16, width: 16 }} /> PLAN
+  <img src="/images/plan.svg" alt="plan" style={{    height: 16,    width: 16,   filter: isDark ? "brightness(0) invert(1)" : "invert(27%) sepia(89%) saturate(400%) hue-rotate(116deg) brightness(96%) contrast(101%)" }} /> PLAN
 </button>
             </div>
           </>
