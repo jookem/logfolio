9478da92c8be096346ea1fa41bd0363ab4836e65
jookem <tegamikureru@hotmail.com>
2026-03-16 16:10:15 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1548,7 +1548,7 @@ function TradeFormModal({ initial, onClose, onSave, onCSVImport, t }) {
   return (
     <div
       style={{
-        position: "absolute",
+        position: "fixed",
         top: 0,
         left: 0,
         right: 0,
