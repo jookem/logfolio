bd4d55afce4fac18e2b28f984c347ecf9f28347c
jookem <tegamikureru@hotmail.com>
2026-03-17 15:23:08 +0900

    Update analyse.js

--- api/analyse.js
+++ api/analyse.js
@@ -15,8 +15,8 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    res.status(200).json(data);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 }
