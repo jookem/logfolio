453f1b982f10f655c816f5834fdfb8422479a9d2
jookem <tegamikureru@hotmail.com>
2026-03-17 10:21:25 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1616,7 +1616,7 @@ function TradeFormModal({ initial, onClose, onSave, onCSVImport, t }) {
 >
   <path d="M20 14V7C20 5.34315 18.6569 4 17 4H12M20 14L13.5 20M20 14H15.5C14.3954 14 13.5 14.8954 13.5 16V20M13.5 20H7C5.34315 20 4 18.6569 4 17V12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   <path d="M7 4V7M7 10V7M7 7H4M7 7H10" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg
+</svg>
     Log A Trade
   </>
 )}
